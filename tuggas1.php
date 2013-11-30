bb6eb1a70f1a3d240af92d05a8f7cb3052a200d3
WidyaSTI <widyararasati@yahoo.com>
2013-11-30 19:43:13 +0700

    Create tuggas1.php

---
+++ tuggas1.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ Title: Membaca file xml.
+	GET xmlfile 
+ */
+require_once 'restler.php';
+require_once 'tugas1class.php';
+
+spl_autoload_register('spl_autoload');
+
+$r = new Restler();
+$r->setSupportedFormats('JsonFormat', 'XmlFormat');
+$r->addAPIClass('Baca');
+$r->handle();
